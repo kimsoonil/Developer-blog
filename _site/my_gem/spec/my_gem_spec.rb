fa0b17347665775c57dc561f6428b8a39aaf2091
Kim-SunIl <sunil.kim@softwareinlife.com>
2021-01-21 10:17:58 +0900

    테마 적용

---
+++ _site/my_gem/spec/my_gem_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MyGem do
+  it "has a version number" do
+    expect(MyGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
